992e4ab72b6b8a6cbbfc7c7516147dd346792ee7
aotemiao <aotemiaox@gmail.com>
2022-04-08 00:13:13 +0800

    使用栈

--- path-sum-ii/path-summ-ii.py
+++ path-sum-ii/path-summ-ii.py
@@ -11,6 +11,7 @@ class TreeNode:
 
 class Solution:
     def pathSum(self, root: TreeNode, targetsum: int) -> bool:
+
         if not root:
             return []
 
@@ -22,14 +23,24 @@ class Solution:
 
         while stack:
             cur_node, path_sum, path_list = stack.pop()
-            temp_list = path_list
+
             if not cur_node.left and not cur_node.right:
                 if path_sum == targetsum:
-                    res.append(temp_list)
+                    res.append(path_list)
             if cur_node.left:
-                stack.append((cur_node.left, path_sum + cur_node.left.val, temp_list.append(cur_node.left.val)))
+                temp_list = path_list[:]
+                temp_list.append(cur_node.left.val)
+                stack.append((cur_node.left, path_sum + cur_node.left.val, temp_list))
 
             if cur_node.right:
-                stack.append((cur_node.right, path_sum + cur_node.right.val, temp_list.append(cur_node.right.val)))
+                temp_list = path_list[:]
+                temp_list.append(cur_node.right.val)
+                stack.append((cur_node.right, path_sum + cur_node.right.val, temp_list))
 
         return res
+
+
+if __name__ == '__main__':
+    print(Solution().pathSum(root=TreeNode(5, TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2)), None),
+                                           TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1)))),
+                             targetsum=22))
